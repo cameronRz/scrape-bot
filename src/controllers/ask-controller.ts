778bc0134f13d9764b774292e043c147df8bf520
Cameron Ramirez <caribcoded@gmail.com>
2023-12-04 15:38:38 -0500

    Update validation

--- src/controllers/ask-controller.ts
+++ src/controllers/ask-controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express';
 import { XataService } from '../services/xata-service';
+import { validationResult } from 'express-validator';
 
 const xataService = new XataService();
 
 export class AskController {
   async ask(req: Request, res: Response) {
-    const question = req.body.question;
-    const result = await xataService.ask(question);
+    const result = validationResult(req);
 
-    res.json(result);
+   if (!result.isEmpty()) {
+     return res.json({ reply: 'I didn\'t catch that.' });
+   }
+
+    const question = req.body.question;
+    const reply = await xataService.ask(question);
+
+    res.json({ reply });
   }
 }
